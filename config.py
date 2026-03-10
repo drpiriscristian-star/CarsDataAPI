57842294a659d85f3f9a0aa2a95965f37513ff77
drpiriscristian-star <drpiriscristian@gmail.com>
2026-03-09 21:47:12 -0300

    Update config.py

--- config.py
+++ config.py
@@ -19,9 +19,9 @@ GOOGLE_CREDENTIALS = {
 SPREADSHEET_ID = os.getenv("GOOGLE_SHEETS_ID")
 
 # Configuración JWT
-JWT_SECRET = os.getenv("JWT_SECRET")
-JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
-JWT_EXPIRATION_MINUTES = int(os.getenv("JWT_EXPIRATION_MINUTES"))
+JWT_SECRET = os.getenv("JWT_SECRET", "paseseguro2026clave")
+JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
+JWT_EXPIRATION_MINUTES = int(os.getenv("JWT_EXPIRATION_MINUTES", "60"))
 
 # Administradores
 ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
